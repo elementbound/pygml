ec0310432b08018205378b55fd27eb9e5b2d0c0b
Gálffy Tamás <ezittgtx@gmail.com>
2017-05-10 01:21:59 +0300

    Added walk_file as utility

--- pygml/visitor/__init__.py
+++ pygml/visitor/__init__.py
@@ -9,8 +9,17 @@ class ExpressionVisitor(LiteralsVisitor, OperatorsVisitor, SubscriptVisitor):
 
     def visit_code(self, source, mode='eval'):
         import ast
-        
+
         source = ast.parse(source, filename='<string>', mode=mode)
+        return self.visit(source)
+
+    def visit_file(self, file, mode='eval'):
+        import ast
+
+        with open(file, 'r') as f:
+            source = f.read()
+
+        source = ast.parse(source, filename=file, mode=mode)
         return self.visit(source)
 
 __all__ = []
